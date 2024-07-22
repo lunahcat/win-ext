c6a129e1d02970a7e7e28bb7bad0828ec836fc00
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2024-07-22 18:30:17 +0900

    Add files via upload

---
+++ Services.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+@REM Diagnostic
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\diagsvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DPS" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WdiServiceHost" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WdiSystemHost" /v "Start" /t REG_DWORD /d "4" /f
+
+@REM Data Usage
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\DusmSvc" /v "Start" /t REG_DWORD /d "4" /f
