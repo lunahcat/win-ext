69f4aca2a59d5a4ceb91485c092a9c47ea6763c7
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2024-08-30 03:16:26 +0900

    Delete Tweaks.cmd

--- Tweaks.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-@REM Delete home navigation from explorer
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace_36354489\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" /f
