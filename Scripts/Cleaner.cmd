eebe0ce9064a9915c92ae2c2f2f03810ad1719f4
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2024-08-30 03:17:04 +0900

    Add files via upload

---
+++ Scripts/Cleaner.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+del /f /s /q "%windir%\Temp"
+del /f /s /q "%localappdata%\Temp"
+
+del /f /s /q "%windir%\Prefetch"
---
+++ Scripts/Uninstall.ps1
@@ -0,0 +1,8 @@
+Get-AppxPackage -Name "Microsoft.WindowsTerminal" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.Windows.Photos" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.ZuneMusic" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.GamingApp" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.XboxGamingOverlay" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "MicrosoftCorporationII.QuickAssist" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.Windows.DevHome" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -Name "Microsoft.OutlookForWindows" | Remove-AppxPackage -AllUsers
