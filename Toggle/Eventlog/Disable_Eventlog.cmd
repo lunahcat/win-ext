664f3f960514bd43318f19bc5f9ef58fb5e70471
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2025-01-04 03:34:24 +0900

    Delete Toggle/Eventlog directory

--- Toggle/Eventlog/Disable_Eventlog.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-sc stop EventLog
-sc stop Wecsvc
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\EventLog" /v "Start" /t REG_DWORD /d "4" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Wecsvc" /v "Start" /t REG_DWORD /d "4" /f
-
-del /f /s /q "%windir%\System32\winevt\Logs"
--- Toggle/Eventlog/Enable_Eventlog.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\EventLog" /v "Start" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\Wecsvc" /v "Start" /t REG_DWORD /d "3" /f
