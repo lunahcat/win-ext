c153fdd71bc049ec2669896904f1f15a89db2a34
Lunahcat <173910594+lunahcat@users.noreply.github.com>
2025-03-02 00:08:06 +0900

    Update Cleaner.cmd

--- Scripts/Cleaner.cmd
+++ Scripts/Cleaner.cmd
@@ -2,5 +2,15 @@
 
 del /f /s /q "%windir%\Temp"
 del /f /s /q "%localappdata%\Temp"
-
 del /f /s /q "%windir%\Prefetch"
+
+wevtutil clear-log Application
+wevtutil clear-log Security
+wevtutil clear-log System
+wevtutil clear-log Setup
+wevtutil clear-log ForwardedEvents
+wevtutil clear-log Microsoft-Windows-PowerShell/Operational
+wevtutil clear-log Microsoft-Windows-EventCollector/Operational
+wevtutil clear-log Microsoft-Windows-TaskScheduler/Operational
+
+ipconfig /flushdns
